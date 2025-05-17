68a979f50053c3113db0528c062c1a7f904cf7e8
joao.marcos.cardoso <joao.cardoso@sharkdev.com.br>
2025-05-17 19:22:33 -0300

    Day 1

---
+++ Day 001/main.py
@@ -0,0 +1,4 @@
+print('Welcome to the Random Band Name Generator!')
+city = input('Please, insert the name of your home town!\n')
+pet = input('Alright! Now, please, insert the name of your pet!\n')
+print(f'Ok! Here is your band name: \n{city} {pet}')
